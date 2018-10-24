d4193d843801e04b1eed081aea9031c949f8304f
luoqua <1132228219@qq.com>
2018-10-24 14:12:07 +0800

    update

--- Packages/Vue/samples/arrow.vue
+++
@@ -1,8 +0,0 @@
-<!--
-This is a bug in the current implementation. Ideally should be fixed
-in a rewrite using sublime-syntax format.
--->
-
-<template>
-  <div @click="foo => foo()"></div>
-</template>
